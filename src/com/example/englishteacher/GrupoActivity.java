c5a3aaeb0552004fb59a602272348f735fe4a4d2
Laura Sánchez <laura0luc@gmail.com>
2012-08-29 18:08:27 -0500

    Sin error de resources

---
+++ src/com/example/englishteacher/GrupoActivity.java
@@ -0,0 +1,13 @@
+package com.example.englishteacher;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class GrupoActivity extends ListActivity {
+
+	@Override
+	  public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.grupo_list);
+	    }
+}
